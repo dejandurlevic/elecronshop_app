d93570dff6928a19039a32cbba949992db64ff2f
dejandurlevic <dejandurlevic@hotmail.com>
2023-12-23 16:41:25 +0100

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className='text-center my-[30px] text-mainBlue font-bold  text-3xl'>Go to sign in</h1>
+    </div>
+  )
+}
+
+export default HomePage
