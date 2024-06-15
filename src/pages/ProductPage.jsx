5c4ea4d1276237a8ffece5e2c647b9faa043ec30
dejandurlevic <dejandurlevic@hotmail.com>
2024-06-15 20:38:25 +0200

    Initial commit

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import CategoryComponents from '../components/CategoryComponents'
+import ProductService from '../services/productService'
+import { useDispatch, useSelector } from 'react-redux'
+import { productsHandler } from '../store/productSlice';
+import SingleCardComponents from '../components/SingleCardComponents';
+
+function ProductPage() {
+
+    const { products, currentCategory } = useSelector(state => state.productStore)
+    const {user } = useSelector((state) => state.userStore)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        ProductService.getAllProducts()
+            .then(res => dispatch(productsHandler(res.data.products)))
+            .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => {
+        ProductService.getProductByCategory(currentCategory)
+            .then(res => {
+                dispatch(productsHandler(res.data.products))
+            })
+            .catch(err => console.log(err))
+    }, [currentCategory])
+
+    return (
+        <div>
+            {user?.firstName === 'Emily' ?  
+             <div>
+            <CategoryComponents />
+
+            <div className='container mx-auto'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-[50px] gap-3 place-items-center'>
+                    {products.map(product => {
+                        return <SingleCardComponents key={product.id} product={product} />
+                    })}
+                </div>
+            </div>
+            </div> : <div> 
+                <h1 className='text-center my-[30px] text-mainBlue font-bold  text-3xl'>To see all products you must first sign in</h1>
+                </div>}
+          
+        </div>
+    )
+}
+
+export default ProductPage
